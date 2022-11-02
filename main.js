3dcdbf910b2ae9faca1b3ec7f9275a8ea534e20a
PaoloNapoli97 <paolonapoli@live.it>
2022-11-02 17:07:05 +0100

    Add excercise without bonus

--- main.js
+++ main.js
@@ -2,13 +2,17 @@
 
 const campContainer = document.querySelector(".camp-container");
 
-for (let i = 1; i <= 100; i++) {
-    const campCell = document.createElement("div");
-    campCell.innerHTML = i;
-    campCell.classList.add("camp-cell-10");
-    campCell.addEventListener("click", function(){
-        console.log(this);
-        this.classList.add("cell-click");
-    })
-    campContainer.append(campCell);
-}
+document.getElementById('play').addEventListener('click', function(){
+        campContainer.innerHTML = "";
+        for (let i = 1; i <= 100; i++) {
+            const campCell = document.createElement("div");
+            campCell.innerHTML = i;
+            campCell.classList.add("camp-cell-10");
+            campCell.addEventListener("click", function(){
+                console.log(this);
+                this.classList.add("cell-click");
+            })
+            campContainer.append(campCell);
+        }
+})
+
